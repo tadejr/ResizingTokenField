15528759fa213a5d4e0912f2be1c52b37d443399
Tadej Razborsek <razborsek.tadej@gmail.com>
2019-06-19 16:01:24 +0200

    Initialized project.

---
+++ ResizingTokenField/ResizingTokenField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResizingTokenField
+//
+//  Created by Tadej Razborsek on 19/06/2019.
+//  Copyright © 2019 Tadej Razborsek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
